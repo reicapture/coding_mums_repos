741241798da2deabbd9dffe9d8a1270142e838ec
reicapture <renata.maringka@gmail.com>
2016-04-13 09:21:58 +0700

    ulang repo

---
+++ js/script.js
@@ -0,0 +1,31 @@
+﻿function readMore() {
+	var temp = document.getElementById("readMore").style.display;
+	if(temp=="none")
+		document.getElementById("readMore").style.display = "block";
+	else
+		document.getElementById("readMore").style.display = "none";
+}
+
+
+function readMore1() {
+	var temp = document.getElementById("readMore1").style.display;
+	if(temp=="none")
+		document.getElementById("readMore1").style.display = "block";
+	else
+		document.getElementById("readMore1").style.display = "none";
+}
+
+
+function readMore2() {
+	var temp = document.getElementById("readMore2").style.display;
+	if(temp=="none")
+		document.getElementById("readMore2").style.display = "block";
+	else
+		document.getElementById("readMore2").style.display = "none";
+}
+
+
+//
+function readMore2() {
+		var temp = document.getElementsByName
+//
